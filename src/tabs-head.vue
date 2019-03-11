0a1a8943a34a82ba613ecf905671bfef510deab6
Forl <kumaloveslife@gmail.com>
2019-03-12 01:21:33 +0800

    完善 tabs 的样式

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -25,11 +25,13 @@
 <style lang="scss" scoped>
   $tab-height: 40px;
   $line-color: blue;
+  $border-color: #ddd;
   .tabs-head {
     display: flex;
     height: $tab-height;
     justify-content: start;
     position: relative;
+    border-bottom: 1px solid $border-color;
     > .line {
       position: absolute;
       bottom: 0;
@@ -38,6 +40,10 @@
     }
     > .actions-wrapper {
       margin-left: auto;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 0 1em;
     }
   }
 </style>
