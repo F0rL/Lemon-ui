67b96b8a52a0f148a2020c422251c1b5be389ff2
Forl <kumaloveslife@gmail.com>
2019-03-12 01:25:59 +0800

    支持 disabled

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tabs-item" @click="xxx" :class="classes">
+  <div class="tabs-item" @click="onClick" :class="classes">
     <slot></slot>
   </div>
 </template>
@@ -33,12 +33,14 @@
     computed: {
       classes() {
         return {
-          active: this.active
+          active: this.active,
+          disabled: this.disabled
         }
       }
     },
     methods: {
-      xxx () {
+      onClick () {
+        if(this.disabled) {return}
         this.eventBus.$emit('update:selected', this.name, this)
       }
     }
@@ -46,6 +48,7 @@
 </script>
 <style lang="scss" scoped>
   $item-color: blue;
+  $disabled-text-color: grey;
   .tabs-item {
     flex-shrink: 0;
     padding: 0 2em;
@@ -56,6 +59,9 @@
     &.active {
       color: $item-color;
       font-weight: bold;
+    }
+    &.disabled {
+      color: $disabled-text-color;
     }
   }
 </style>
