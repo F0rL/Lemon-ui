c78f3f2c0cc4260f170e193aa5495ce74c14f659
Forl <kumaloveslife@gmail.com>
2019-03-13 10:22:34 +0800

    完成 collapse 组件

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="collapseItem">
-    <div class="title" @click="toggle">
+    <div class="title" @click="toggle" :data-name="name">
       {{title}}
     </div>
-    <div class="content" v-if="open">
+    <div class="content" ref="content" v-if="open">
       <slot></slot>
     </div>
   </div>
@@ -19,70 +19,50 @@
       },
       name: {
         type: String,
-        require: true
+        required: true
       }
     },
-    data() {
+    data () {
       return {
-        open: false
+        open: false,
       }
     },
     inject: ['eventBus'],
-    mounted() {
-      this.eventBus && this.eventBus.$on('update:selected', (name) => {
-        if(name !== this.name) {
-          this.close()
-        }else {
-          this.show()
+    mounted () {
+      this.eventBus && this.eventBus.$on('update:selected', (names) => {
+        if (names.indexOf(this.name) >= 0) {
+          this.open = true
+        } else {
+          this.open = false
         }
       })
     },
     methods: {
-      toggle() {
-        if(this.open) {
-          this.open = false
-        }else {
-          this.eventBus && this.eventBus.$emit('update:selected', this.name)
+      toggle () {
+        if (this.open) {
+          this.eventBus && this.eventBus.$emit('update:removeSelected', this.name)
+        } else {
+          this.eventBus && this.eventBus.$emit('update:addSelected', this.name)
         }
       },
-      close() {
-        this.open = false
-      },
-      show() {
-        this.open = true
-      }
-    }
+    },
   }
 </script>
 
 <style scoped lang="scss">
-  $border-color: grey;
+  $grey: #ddd;
   $border-radius: 4px;
   .collapseItem {
-    > .title {
-      border: 1px solid $border-color;
-      margin-top: -1px;
-      margin-left: -1px;
-      margin-right: -1px;
-      min-height: 32px;
-      display: flex;
-      align-items: center;
-      padding: 0 8px;
+    > .title { border: 1px solid $grey; margin-top: -1px; margin-left: -1px; margin-right: -1px;
+      min-height: 32px; display: flex; align-items: center; padding: 0 8px;
+      background: lighten($grey, 8%);
     }
     &:first-child {
-      > .title {
-        border-top-left-radius: $border-radius;
-        border-top-right-radius: $border-radius;
-      }
+      > .title { border-top-left-radius: $border-radius; border-top-right-radius: $border-radius; }
     }
     &:last-child {
-      > .title:last-child {
-        border-bottom-left-radius: $border-radius;
-        border-bottom-right-radius: $border-radius;
-      }
+      > .title:last-child { border-bottom-left-radius: $border-radius; border-bottom-right-radius: $border-radius; }
     }
-    > .content {
-      padding: 0 8px;
-    }
+    > .content { padding: 8px; }
   }
 </style>
